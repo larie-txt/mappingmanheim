35e85695e015e32fb5005919fd93530edb264be0
larie-txt <123500434+larie-txt@users.noreply.github.com>
2026-05-26 11:31:49 +0200

    Update NFC_script.js

--- NFC_script.js
+++ NFC_script.js
@@ -41,15 +41,7 @@ async function scanNFC() {
       }
     });
 
-  	window.addEventListener("keydown", (e) => {
-      if (!window.cablesPatch) return;
-
-      if (e.key >= "0" && e.key <= "9") {
-        const numberValue = Number(e.key);
-          window.cablesPatch.setVariable("nfcNumber", numberValue);
-          console.log("Test value sent to cables:", numberValue);
-    }
-});
+  	
     
   } catch (error) {
     console.error("NFC scan failed:", error);
@@ -57,5 +49,19 @@ async function scanNFC() {
 }
 
 document.addEventListener("click", () => {
+    scanNFC();
+}, { once: true });
+
+
+// temporary desktop test fallback
+window.addEventListener("keydown", (e) => {
+    if (!window.cablesPatch) return;
+
+    if (e.key >= "0" && e.key <= "9") {
+        const numberValue = Number(e.key);
+        window.cablesPatch.setVariable("nfcNumber", numberValue);
+        console.log("Test value sent to cables:", numberValue);
+    }
+});
   scanNFC();
 }, { once: true });
